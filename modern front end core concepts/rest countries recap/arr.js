0c898edf343bfcba64258b6f94db59f1ac4c5779
MirzaAyon <ayonmirza98@gmail.com>
2022-08-01 14:57:51 +0600

    map used in react to explore countries

---
+++ modern front end core concepts/rest countries recap/arr.js
@@ -0,0 +1,8 @@
+const arr = ['ayon', 'plabon', 'dui', 'bhai'];
+let ans = arr.join(); //by default comma diye join hbe 
+console.log(ans);
+let ans2 = arr.join(" ");
+console.log(ans2);
+let ans3 = arr.join('#');
+console.log(ans3);
+//arr er modhe onk gula jinish thakle eke join diye ekta te banae felte pari
