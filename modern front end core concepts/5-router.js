9683fae43e171ffdca2cb7e6dce56aa5ec28e11e
MirzaAyon <ayonmirza98@gmail.com>
2022-08-01 17:44:53 +0600

    router concept added

---
+++ modern front end core concepts/5-router.js
@@ -0,0 +1,19 @@
+// gmail - mail - settings - Labels e giye
+
+// egula inspact e dibo ek ek kore
+// location
+// Location {ancestorOrigins: DOMStringList, href: 'https://mail.google.com/mail/u/0/#settings/labels', origin: 'https://mail.google.com', protocol: 'https:', host: 'mail.google.com', …}ancestorOrigins: DOMStringList {length: 0}assign: ƒ assign()hash: "#settings/labels"host: "mail.google.com"hostname: "mail.google.com"href: "https://mail.google.com/mail/u/0/#settings/labels"origin: "https://mail.google.com"pathname: "/mail/u/0/"port: ""protocol: "https:"reload: ƒ reload()replace: ƒ replace()search: ""toString: ƒ toString()valueOf: ƒ valueOf()Symbol(Symbol.toPrimitive): undefined[[Prototype]]: Location
+// location.hash
+// '#settings/labels'
+// location.hash = '#settings/inbox'
+// '#settings/inbox'
+
+//tkhn inbox e niye jabe
+
+//evabe ekek kore offline , general e jabo
+//ei kaj ta history API diyeo kore
+//routing - user kon kon jinish gula dekhbe ta depend kore route er upore
+//url er porer ongsho take route bole ekhane query o thakte pare hash o thakte pare
+
+//single page application e route name ekta jinish ache , route er upr depend kore user kon jinish ta dekhte chae ta dekhte paae
+//route er maddhhome ei same page er modhe bivinno jinish gulake change kore dekhae
