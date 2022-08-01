ea5b1378812e63ae6f26d95de118b0860adbf5d1
MirzaAyon <ayonmirza98@gmail.com>
2022-08-01 21:14:29 +0600

    destructure attribute added

---
+++ modern front end core concepts/6-note.js
@@ -0,0 +1,27 @@
+// const student = { name : 'ayon', roll : 9}
+// undefined
+// student.name
+// 'ayon'
+// student.roll
+// 9
+// name
+// ''
+// roll
+// error dibe
+
+
+//ami chaile name and age name variable declare kore felte pari
+//destructuring kore
+
+const { name, age } = student; //orthath student object er vitore name namer jei property ta ache shetai hbe ei name property
+undefined
+name
+'ayon'
+age
+//thik temon e 
+// const { name, flags } = country
+//name.common
+//flags.png
+
+
+
