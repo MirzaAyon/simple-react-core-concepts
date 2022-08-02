1174dec450ed9bbe65a0b7786cc4d874573e9fe5
MirzaAyon <ayonmirza98@gmail.com>
2022-08-02 15:32:28 +0600

    some quiz added for front end

---
+++ modern front end core concepts/quiz.js
@@ -0,0 +1,29 @@
+// 3 - What does window.location.href return?
+// ans : returns the href (url) of current page
+//rendering er meaning holo tortaja
+
+9
+// https://github.com/MirzaAyon/simple-react-core-concepts
+
+// location.pathname
+// dile ki hbe?console e ?
+
+// ans:
+// '/MirzaAyon/simple-react-core-concepts'
+
+//10:What is routing?
+// Define the URL and based on the URL set different parts of the website
+
+// In a SPA, a page refresh ……………
+// never occures
+
+//12
+// Which one is not an Array method ?
+
+// array method :slice(), splice(), some()
+// non array method : ChannelSplitterNode()
+
+// 13
+// Which one is not an Array method ?
+
+// ans loads a new document
