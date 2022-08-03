17b037aeaa4fb387830182415dc8191bc4b9ee7d
MirzaAyon <ayonmirza98@gmail.com>
2022-08-03 20:47:27 +0600

    quiz added

---
+++ react-core-concepts-JSX-PROPS-States/quiz.js
@@ -0,0 +1,8 @@
+//4
+//How will create a state with a default value “JingaLala”?
+// ans : const [name, setName] = useState(“JingaLala”);
+
+
+//13
+// How would you pass data to a component?
+// Using props
