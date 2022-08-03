d59da5c72b0f72d9caedfc36ef619caa21566cfb
MirzaAyon <ayonmirza98@gmail.com>
2022-08-03 16:03:59 +0600

    destructuring added

---
+++ react-core-concepts-JSX-PROPS-States/array-object-des-concept.js
@@ -0,0 +1,14 @@
+// {name, age}={name :'a', age :20}
+// {name: 'a', age: 20}
+// name
+// 'a'
+// age
+// 20
+// [first,second] = [10,20]
+// (2) [10, 20]
+// first
+// 10
+// second
+// 20
+
+//egula console e try krbo
