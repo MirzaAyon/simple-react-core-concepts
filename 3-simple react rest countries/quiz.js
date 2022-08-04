c1e092d9c46e81f85df29477792e2c0f6605cd6c
MirzaAyon <ayonmirza98@gmail.com>
2022-08-04 21:15:17 +0600

    some quiz added

---
+++ 3-simple react rest countries/quiz.js
@@ -0,0 +1,37 @@
+//5
+react hooks:
+16 ta
+useCallback
+useContext
+useDebugValue
+useDeferredValue
+useEffect
+useId
+useImperativeHandle
+useInsertionEffect
+useLayoutEffect
+useMemo
+useReducer
+useRef
+useState
+useSyncExternalStore
+useTransition
+
+
+//7
+// Why do we use the useState hook in a react component?
+//to set the loaded data from the API in a state variable
+
+//13
+// What does useEffect() hook do in react?
+//It lets you perform side effects in components
+//14
+//What does useState() returns ?
+//a stateful value and a function to update it
+
+//15
+// What is a Hook in React?
+// A Hook is a special function that lets you “hook into” React features.
+
+
+//hook gula shomporke janbo eta important
